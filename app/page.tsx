dd9cd006a13f5b9b508054b474aa0087510da3ba
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-02 08:59:44 +0545

    Ui fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,5 @@
 import Board from "../components/Board";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-start justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="w-full overflow-x-auto">
-        <Board />
-      </main>
-    </div>
-  );
+  return <Board />;
 }
