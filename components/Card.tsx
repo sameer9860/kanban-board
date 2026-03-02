a6122982ba390e3a23547cfd0583220767de9f0e
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-02 08:51:00 +0545

    Updated card'

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,13 +1,38 @@
 import React from "react";
 import { Card as CardType } from "../types/board";
+import {
+  useSortable,
+  defaultAnimateLayoutChanges,
+  AnimateLayoutChanges,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 interface CardProps {
   card: CardType;
 }
 
 const Card: React.FC<CardProps> = ({ card }) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id: card.id });
+
+  const style: React.CSSProperties = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
   return (
-    <div className="bg-white shadow rounded p-2 mb-2">
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className="bg-white shadow rounded p-2 mb-2 cursor-move"
+    >
       {card.title}
     </div>
   );
